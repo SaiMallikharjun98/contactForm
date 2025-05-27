6b7af07a49eb47f10e223b0ff100b5243f806107
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-27 22:33:53 +0530

    Styles Changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   };
   return (
     <>
-      <div className="w-[600px] h-screen mx-auto my-24 bg-black text-center px-4">
+      <div className="w-[550px] h-screen mx-auto my-24 bg-black text-center px-4 py-4">
         <h1 className="text-2xl font-bold text-white">Contact Me</h1>
         
           <form action="" onSubmit={handleSubmit(submitHandler)}>
@@ -17,19 +17,19 @@ function App() {
               {...register("name")}
               type="text"
               placeholder="Enter Your Name"
-              className="bg-white p-2 rounded-2xl w-[300px] text-xl mt-4"
+              className="bg-white p-2 rounded-2xl w-[350px] text-xl mt-4"
             />
             <input
               {...register("mail")}
               type="text"
               placeholder="Enter Your Mail"
-              className="bg-white p-2 rounded-2xl w-[300px] text-xl mt-4"
+              className="bg-white p-2 rounded-2xl w-[350px] text-xl mt-4"
             />
             <textarea
               {...register("message")}
               type="text"
               placeholder="Enter Your Message"
-              className="bg-white p-2 rounded-2xl w-[300px] text-xl mt-4"
+              className="bg-white p-2 rounded-2xl w-[350px] text-xl mt-4"
             ></textarea>
             <input
               type="submit"
